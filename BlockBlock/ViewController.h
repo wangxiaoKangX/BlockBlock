e6b8a853eeb0f16e1383b0846dac4cae5aaaa62d
wangxkK <593976841@qq.com>
2017-05-03 21:21:12 +0800

    Initial Commit

---
+++ BlockBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockBlock
+//
+//  Created by 王晓康 on 2017/5/3.
+//  Copyright © 2017年 wangxiaokang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlockBlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockBlock
+//
+//  Created by 王晓康 on 2017/5/3.
+//  Copyright © 2017年 wangxiaokang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
